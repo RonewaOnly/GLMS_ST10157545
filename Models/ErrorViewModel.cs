8d3d76d8223ca4bed391f5aff61abbe913e37f62
Student-Ronewa <Student@LabVM2049826>
2026-05-02 22:36:17 +0200

    Replace dev project with test project branch

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace GLMS_ST10157545.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
