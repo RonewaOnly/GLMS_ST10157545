3ae4dbf0cf77f6d54bbfef9a2d22f1a8baecfdfa
Student-Ronewa <Student@LabVM2049826>
2026-05-02 22:50:47 +0200

    Add initial controllers for Clients, Contracts, and ServiceRequests
    Added ClientsController, ContractsController, and ServiceRequestsController in the GLMS_ST10157545.Controllers namespace. Each controller includes an Index action that returns a view, establishing the basic structure for handling requests related to clients, contracts, and service requests in the ASP.NET MVC application.

---
+++ Controllers/ClientsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GLMS_ST10157545.Controllers
+{
+    public class ClientsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/ContractsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GLMS_ST10157545.Controllers
+{
+    public class ContractsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
